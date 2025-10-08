0195c3ebd4b5bc06032e166345e53b6d4a8d140d
Kalp-Shah9820 <shahkalp9820@gmail.com>
2025-10-08 20:53:12 +0530

    Changing male voice to female and adjusting the speed

--- roboSpeaker.py
+++ roboSpeaker.py
@@ -4,12 +4,14 @@ if __name__ == "__main__":
     engine = pyttsx3.init()
     print("Welcome to RoboSpeaker! created by Kalp Shah")
     while True:
+        engine.setProperty('rate', 150)  # Speed (default ~200)
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[1].id)  # Use female voice (optional)
         x=input("Enter what u want me to say: ")
         if x=="q":
             engine.say("'Goodbye'")
             engine.runAndWait()
             break
-        engine.setProperty('rate', 150)  
 
         engine.say(x)
         engine.runAndWait()
